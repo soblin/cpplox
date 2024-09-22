fdaca82226624b5479acf7e1ecb020717191a3a6
soblin <hilo.soblin@gmail.com>
2024-09-23 06:18:46 +0900

    adding test for column 3
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -262,11 +262,7 @@ auto Tokenizer::add_identifier_token() -> std::optional<ParseError>
 auto Tokenizer::handle_newline() -> void
 {
   line_++;
-<<<<<<< HEAD
   column_ = 0;
-=======
-  column_ = 1;
->>>>>>> efcf3ba (adding test for column)
 }
 
 auto Tokenizer::advance_cursor() -> void
