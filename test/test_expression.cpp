ddf91a49fb6e6e77075b9b8695d36df64971d22f
soblin <hilo.soblin@gmail.com>
2024-09-24 16:47:59 +0900

    long becomes double somehow
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- test/test_expression.cpp
+++ test/test_expression.cpp
@@ -6,22 +6,43 @@
 
 TEST(Evaluate, math)
 {
-  const std::string source = R"(-123 * ((45.67) / (89.0)) - 1.23 + (4.567))";
-  auto tokenizer = lox::Tokenizer(source);
-  const auto result = tokenizer.take_tokens();
-  EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
-  const auto & tokens = lox::as_variant<lox::Tokens>(result);
+  {
+    const std::string source = R"(-123 * ((45 * 67) / (89)) - 123 + (4567))";
+    auto tokenizer = lox::Tokenizer(source);
+    const auto result = tokenizer.take_tokens();
+    EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
+    const auto & tokens = lox::as_variant<lox::Tokens>(result);
 
-  auto parser = lox::Parser(tokens);
-  const auto parse_result = parser.expression();
-  EXPECT_EQ(lox::is_variant_v<lox::Expr>(parse_result), true);
+    auto parser = lox::Parser(tokens);
+    const auto parse_result = parser.expression();
+    EXPECT_EQ(lox::is_variant_v<lox::Expr>(parse_result), true);
 
-  const auto & expr = lox::as_variant<lox::Expr>(parse_result);
-  const auto & eval_opt = lox::evaluate_expr(expr);
-  EXPECT_EQ(lox::is_variant_v<lox::Value>(eval_opt), true);
-  const auto & eval = lox::as_variant<lox::Value>(eval_opt);
-  EXPECT_EQ(lox::is_variant_v<double>(eval), true);
-  EXPECT_FLOAT_EQ(lox::as_variant<double>(eval), -123 * ((45.67) / (89.0)) - 1.23 + (4.567));
+    const auto & expr = lox::as_variant<lox::Expr>(parse_result);
+    const auto & eval_opt = lox::evaluate_expr(expr);
+    EXPECT_EQ(lox::is_variant_v<lox::Value>(eval_opt), true);
+    const auto & eval = lox::as_variant<lox::Value>(eval_opt);
+    std::cout << eval.index() << std::endl;
+    EXPECT_EQ(lox::is_variant_v<double>(eval), true);
+    EXPECT_FLOAT_EQ(lox::as_variant<double>(eval), -123 * ((45 * 67) / (89)) - 123 + (4567));
+  }
+  {
+    const std::string source = R"(-123 * ((45.67) / (89.0)) - 1.23 + (4.567))";
+    auto tokenizer = lox::Tokenizer(source);
+    const auto result = tokenizer.take_tokens();
+    EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
+    const auto & tokens = lox::as_variant<lox::Tokens>(result);
+
+    auto parser = lox::Parser(tokens);
+    const auto parse_result = parser.expression();
+    EXPECT_EQ(lox::is_variant_v<lox::Expr>(parse_result), true);
+
+    const auto & expr = lox::as_variant<lox::Expr>(parse_result);
+    const auto & eval_opt = lox::evaluate_expr(expr);
+    EXPECT_EQ(lox::is_variant_v<lox::Value>(eval_opt), true);
+    const auto & eval = lox::as_variant<lox::Value>(eval_opt);
+    EXPECT_EQ(lox::is_variant_v<double>(eval), true);
+    EXPECT_FLOAT_EQ(lox::as_variant<double>(eval), -123 * ((45.67) / (89.0)) - 1.23 + (4.567));
+  }
 }
 
 TEST(Evaluate, compare)
