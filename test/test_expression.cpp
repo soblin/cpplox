f31b5e493bb3a4ea2c60a01d15d0035355db4ba2
Mamoru Sobue <hilo.soblin@gmail.com>
2024-09-24 22:56:17 +0900

    feat(token): add long type (#15)
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- test/test_expression.cpp
+++ test/test_expression.cpp
@@ -6,28 +6,51 @@
 
 TEST(Evaluate, math)
 {
-  const std::string source = R"(-123 * ((45.67) / (89.0)) - 1.23 + (4.567))";
-  auto tokenizer = lox::Tokenizer(source);
-  const auto result = tokenizer.take_tokens();
-  EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
-  const auto & tokens = lox::as_variant<lox::Tokens>(result);
+  {
+    // const std::string source = R"(-123 * ((45 * 67) / (89)) - 123 + (4567))";
+    const std::string source = R"(45 * 67)";
+    auto tokenizer = lox::Tokenizer(source);
+    const auto result = tokenizer.take_tokens();
+    EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
+    const auto & tokens = lox::as_variant<lox::Tokens>(result);
 
-  auto parser = lox::Parser(tokens);
-  const auto parse_result = parser.expression();
-  EXPECT_EQ(lox::is_variant_v<lox::Expr>(parse_result), true);
+    auto parser = lox::Parser(tokens);
+    const auto parse_result = parser.expression();
+    EXPECT_EQ(lox::is_variant_v<lox::Expr>(parse_result), true);
 
-  const auto & expr = lox::as_variant<lox::Expr>(parse_result);
-  const auto & eval_opt = lox::evaluate_expr(expr);
-  EXPECT_EQ(lox::is_variant_v<lox::Value>(eval_opt), true);
-  const auto & eval = lox::as_variant<lox::Value>(eval_opt);
-  EXPECT_EQ(lox::is_variant_v<double>(eval), true);
-  EXPECT_FLOAT_EQ(lox::as_variant<double>(eval), -123 * ((45.67) / (89.0)) - 1.23 + (4.567));
+    const auto & expr = lox::as_variant<lox::Expr>(parse_result);
+    const auto & eval_opt = lox::evaluate_expr(expr);
+    EXPECT_EQ(lox::is_variant_v<lox::Value>(eval_opt), true);
+    const auto & eval = lox::as_variant<lox::Value>(eval_opt);
+    EXPECT_EQ(lox::is_variant_v<int64_t>(eval), true);
+    EXPECT_EQ(lox::as_variant<int64_t>(eval), 45 * 67);
+  }
+  {
+    const std::string source = R"(-123 * ((45.67) / (89.0)) - 1.23 + (4.567))";
+    auto tokenizer = lox::Tokenizer(source);
+    const auto result = tokenizer.take_tokens();
+    EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
+    const auto & tokens = lox::as_variant<lox::Tokens>(result);
+
+    auto parser = lox::Parser(tokens);
+    const auto parse_result = parser.expression();
+    EXPECT_EQ(lox::is_variant_v<lox::Expr>(parse_result), true);
+
+    const auto & expr = lox::as_variant<lox::Expr>(parse_result);
+    const auto & eval_opt = lox::evaluate_expr(expr);
+    EXPECT_EQ(lox::is_variant_v<lox::Value>(eval_opt), true);
+    const auto & eval = lox::as_variant<lox::Value>(eval_opt);
+    EXPECT_EQ(lox::is_variant_v<double>(eval), true);
+    EXPECT_FLOAT_EQ(lox::as_variant<double>(eval), -123 * ((45.67) / (89.0)) - 1.23 + (4.567));
+  }
 }
 
 TEST(Evaluate, compare)
 {
   {
-    const std::string source = R"(-123 * ((45.67) / (89.0)) > -124 * ((45.67) / (89.0)))";
+    const std::string source = R"(
+(-123 * ((45.67) / (89.0))) > (-124 * ((45.67) / (89.0)))
+)";
     auto tokenizer = lox::Tokenizer(source);
     const auto result = tokenizer.take_tokens();
     EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
@@ -42,10 +65,11 @@ TEST(Evaluate, compare)
     EXPECT_EQ(lox::is_variant_v<lox::Value>(eval_opt), true);
     const auto & eval = lox::as_variant<lox::Value>(eval_opt);
     EXPECT_EQ(lox::is_variant_v<bool>(eval), true);
-    EXPECT_EQ(lox::as_variant<bool>(eval), -123 * ((45.67) / (89.0)) > -124 * ((45.67) / (89.0)));
+    EXPECT_EQ(
+      lox::as_variant<bool>(eval), (-123 * ((45.67) / (89.0))) >= (-124 * ((45.67) / (89.0))));
   }
   {
-    const std::string source = R"(-123 * ((45.67) / (89.0)) >= -124 * ((45.67) / (89.0)))";
+    const std::string source = R"((-123 * ((45.67) / (89.0))) >= (-124 * ((45.67) / (89.0))))";
     auto tokenizer = lox::Tokenizer(source);
     const auto result = tokenizer.take_tokens();
     EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
@@ -60,7 +84,8 @@ TEST(Evaluate, compare)
     EXPECT_EQ(lox::is_variant_v<lox::Value>(eval_opt), true);
     const auto & eval = lox::as_variant<lox::Value>(eval_opt);
     EXPECT_EQ(lox::is_variant_v<bool>(eval), true);
-    EXPECT_EQ(lox::as_variant<bool>(eval), -123 * ((45.67) / (89.0)) > -124 * ((45.67) / (89.0)));
+    EXPECT_EQ(
+      lox::as_variant<bool>(eval), (-123 * ((45.67) / (89.0))) >= (-124 * ((45.67) / (89.0))));
   }
 
   {
