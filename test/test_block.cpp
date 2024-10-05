b52fb5c0d653a6a27afd9d6b809014c2c8749f80
soblin <hilo.soblin@gmail.com>
2024-10-05 12:53:16 +0900

    add test
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- test/test_block.cpp
+++ test/test_block.cpp
@@ -110,6 +110,54 @@ var c = "global c";
   }
 }
 
+TEST(Block, global_variable_assign)
+{
+  const std::string source1 = R"(
+// first body
+var a = "global a";
+var b = "global b";
+var c = "global c";
+
+// first block
+{
+   var a = "inner1_a";
+   c = "inner1_c";
+   // second block
+   {
+      var a = "inner2_a";
+      var b = "inner2_b";
+      c = "inner2_c";
+   }
+   // end second block
+}
+// end first block
+)";
+  auto tokenizer = lox::Tokenizer(source1);
+  const auto result = tokenizer.take_tokens();
+  EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
+  const auto & tokens = lox::as_variant<lox::Tokens>(result);
+
+  auto parser = lox::Parser(tokens);
+  const auto parse_result = parser.program();
+  EXPECT_EQ(lox::is_variant_v<lox::Program>(parse_result), true);
+  const auto & program = lox::as_variant<lox::Program>(parse_result);
+
+  lox::Interpreter interpreter{};
+  interpreter.execute(program);
+  const auto a_opt = interpreter.get_variable(tokens[1]);
+  EXPECT_EQ(a_opt.has_value(), true);
+  EXPECT_EQ(lox::is_variant_v<std::string>(a_opt.value()), true);
+  EXPECT_EQ(lox::as_variant<std::string>(a_opt.value()), "global a");
+  const auto b_opt = interpreter.get_variable(tokens[6]);
+  EXPECT_EQ(b_opt.has_value(), true);
+  EXPECT_EQ(lox::is_variant_v<std::string>(b_opt.value()), true);
+  EXPECT_EQ(lox::as_variant<std::string>(b_opt.value()), "global b");
+  const auto c_opt = interpreter.get_variable(tokens[11]);
+  EXPECT_EQ(c_opt.has_value(), true);
+  EXPECT_EQ(lox::is_variant_v<std::string>(c_opt.value()), true);
+  EXPECT_EQ(lox::as_variant<std::string>(c_opt.value()), "inner2_c");
+}
+
 int main(int argc, char ** argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
