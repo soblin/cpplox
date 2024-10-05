47b15f99955db5f9cb83fc8be4662d2f5c6f7d6e
Mamoru Sobue <hilo.soblin@gmail.com>
2024-10-05 14:40:22 +0900

    refactor(environment): move impl (#50)
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- include/cpplox/environment.hpp
+++ include/cpplox/environment.hpp
@@ -23,29 +23,9 @@ public:
   }
 
   [[nodiscard]] auto assign(const Token & var, const Value & var_value)
-    -> std::optional<RuntimeError>
-  {
-    const auto key = std::string(var.lexeme);
-    if (const auto it = values_.find(key); it != values_.end()) {
-      values_[key] = var_value;
-      return std::nullopt;
-    }
-    if (enclosing_) {
-      return enclosing_->assign(var, var_value);
-    }
-    return RuntimeError{RuntimeErrorKind::UndefinedVariable};
-  }
+    -> std::optional<RuntimeError>;
 
-  auto get(const Token & name) const -> std::variant<Value, RuntimeError>
-  {
-    if (const auto it = values_.find(std::string(name.lexeme)); it != values_.end()) {
-      return it->second;
-    } else if (enclosing_) {
-      return enclosing_->get(name);
-    } else {
-      return RuntimeError{RuntimeErrorKind::UndefinedVariable};
-    }
-  }
+  auto get(const Token & name) const -> std::variant<Value, RuntimeError>;
 
 private:
   // NOTE: if the key is string_view, even if the string content is same "foo", if the source of the
