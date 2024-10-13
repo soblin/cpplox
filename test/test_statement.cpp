61c01a862c37f46e6a3cdc2e031cbd04b5601565
soblin <hilo.soblin@gmail.com>
2024-10-14 02:41:31 +0900

    test(statement): add test for break/continue
    Signed-off-by: soblin <hilo.soblin@gmail.com>

--- test/test_statement.cpp
+++ test/test_statement.cpp
@@ -1912,6 +1912,96 @@ for (var a = 0; a < 10; a = a + 1) {
     EXPECT_EQ(exec.has_value(), true);
     EXPECT_EQ(lox::is_variant_v<lox::UndefinedVariableError>(exec.value()), true);
   }
+  {
+    const std::string source = R"(
+var a = 0;
+var b = 0;
+var c = 0;
+for (;) {
+  a = a + 1;
+  if (a > 10) {
+    break;
+  }
+  for (;;) {
+    if (b > 10) {
+      break;
+    }
+    b = b + 1;
+  }
+  c = c + 1;
+}
+)";
+    auto tokenizer = lox::Tokenizer(source);
+    const auto result = tokenizer.take_tokens();
+    EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
+    const auto & tokens = lox::as_variant<lox::Tokens>(result);
+
+    auto parser = lox::Parser(tokens);
+    const auto parse_result = parser.program();
+    EXPECT_EQ(lox::is_variant_v<lox::SyntaxError>(parse_result), true);
+    const auto & err = lox::as_variant<lox::SyntaxError>(parse_result);
+    EXPECT_EQ(err.kind, lox::SyntaxErrorKind::InvalidLiteralError);
+  }
+  {
+    const std::string source = R"(
+var a = 0;
+var b = 0;
+var c = 0;
+for (;;) {
+  a = a + 1;
+  if (a > 10) {
+    break
+  }
+  for (;;) {
+    if (b > 10) {
+      break;
+    }
+    b = b + 1;
+  }
+  c = c + 1;
+}
+)";
+    auto tokenizer = lox::Tokenizer(source);
+    const auto result = tokenizer.take_tokens();
+    EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
+    const auto & tokens = lox::as_variant<lox::Tokens>(result);
+
+    auto parser = lox::Parser(tokens);
+    const auto parse_result = parser.program();
+    EXPECT_EQ(lox::is_variant_v<lox::SyntaxError>(parse_result), true);
+    const auto & err = lox::as_variant<lox::SyntaxError>(parse_result);
+    EXPECT_EQ(err.kind, lox::SyntaxErrorKind::StmtWithoutSemicolun);
+  }
+  {
+    const std::string source = R"(
+var a = 0;
+var b = 0;
+var c = 0;
+for (;;) {
+  a = a + 1;
+  if (a > 10) {
+    break;
+  }
+  for (;;) {
+    if (b < 10) {
+      continue
+    }
+    b = b + 1;
+  }
+  c = c + 1;
+}
+)";
+    auto tokenizer = lox::Tokenizer(source);
+    const auto result = tokenizer.take_tokens();
+    EXPECT_EQ(lox::is_variant_v<lox::Tokens>(result), true);
+    const auto & tokens = lox::as_variant<lox::Tokens>(result);
+
+    auto parser = lox::Parser(tokens);
+    const auto parse_result = parser.program();
+    EXPECT_EQ(lox::is_variant_v<lox::SyntaxError>(parse_result), true);
+    const auto & err = lox::as_variant<lox::SyntaxError>(parse_result);
+    EXPECT_EQ(err.kind, lox::SyntaxErrorKind::StmtWithoutSemicolun);
+  }
 }
 
 int main(int argc, char ** argv)
